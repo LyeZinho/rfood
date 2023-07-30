9ab1354b5a94b01a160f5b73a1cc97669e7b6f48
Pedro Kaleb! <73436445+LyeZinho@users.noreply.github.com>
2023-07-30 11:24:53 +0100

    Update SuportButton.jsx

--- src/components/SuportButton.jsx
+++ src/components/SuportButton.jsx
@@ -11,7 +11,7 @@ export default function SuportButton() {
                 cursor-pointer hover:border-blue-500 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110
                 animate-bounce
             "
-        onClick={() => window.open("/suport", "_blank")}
+        onClick={() => window.open("/suport")}
       >
         Suport Me :D
       </button>
